0aea5b1e7a6a7486c66cf7a82837cf4de149aa9f
matentzn <nicolas.matentzoglu@gmail.com>
2020-07-01 14:02:41 +0300

    adds short_form index after loading data
    see https://github.com/VirtualFlyBrain/neo4j2owl/issues/28

--- process.sh
+++ process.sh
@@ -27,6 +27,9 @@ cat ${QUERY}
 #curl -i -X POST --data "@${QUERY}" -H "Content-Type: application/json" --user ${user}:${password} -X POST ${server}/db/data/transaction/commit
 # https://neo4j.com/docs/http-api/3.5/actions/begin-and-commit-a-transaction-in-one-request/
 curl -i -X POST ${server}/db/data/transaction/commit -u ${user}:${password} -H 'Content-Type: application/json' -d "@${QUERY}"
+curl -i -X POST ${server}/db/data/transaction/commit -u ${user}:${password} -H 'Content-Type: application/json' -d '{"statements": [{"statement": "CREATE INDEX ON :Individual(short_form)"}]}'
+curl -i -X POST ${server}/db/data/transaction/commit -u ${user}:${password} -H 'Content-Type: application/json' -d '{"statements": [{"statement": "CREATE INDEX ON :Class(short_form)"}]}'
+
 
 #cat ${CYPHER} | cypher-shell -u ${user} -p ${password} -a ${server} --format plain
 
